19b81eb412586961fcce80679f1f22d0fdf484fc
Ricky Chittaphone <rickychittaphone@gmail.com>
2022-10-26 10:23:26 -0400

    Add player dialog
    Can now add player and will update the displayed table

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component, ViewChild } from '@angular/core';
-import {MatTable} from '@angular/material/table';
+import { Component, Inject, ViewChild } from '@angular/core';
+import { MatTable } from '@angular/material/table';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 export interface Player {
   name: string;
   job: string;
@@ -18,12 +19,25 @@ const PLAYER_DATA: Player[] = [
 export class AppComponent {
   displayedColumns: string[] = ['name', 'job'];
   dataSource = [...PLAYER_DATA];
+  currentPlayer: Player = { name:'', job: ''};
 
   @ViewChild(MatTable) table!: MatTable<Player>;
 
+  constructor(public dialog: MatDialog) {}
+
+  openAddPlayerDialog(): void {
+    this.currentPlayer = { name:'', job: ''};
+    const dialogRef = this.dialog.open(DialogNewPlayer, {
+
+      data: {name: this.currentPlayer.name, job: this.currentPlayer.job},
+    });
+    dialogRef.afterClosed().subscribe(result => {
+      this.currentPlayer = result;
+      this.addPlayer()
+    });
+  }
   addPlayer() {
-    const randomPlayerIndex = Math.floor(Math.random() * PLAYER_DATA.length);
-    this.dataSource.push(PLAYER_DATA[randomPlayerIndex]);
+    this.dataSource.push(this.currentPlayer);
     this.table.renderRows();
   }
 
@@ -31,4 +45,23 @@ export class AppComponent {
     this.dataSource.pop();
     this.table.renderRows();
   }
+  selectPlayer() {
+
+  }
+
+}
+
+@Component({
+  selector: 'dialog-new-player',
+  templateUrl: 'dialog-new-player.html',
+})
+export class DialogNewPlayer {
+constructor(
+  public dialogRef: MatDialogRef<DialogNewPlayer>,
+  @Inject(MAT_DIALOG_DATA) public data: Player,
+) {}
+onNoClick(): void {
+  this.dialogRef.close();
+}
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,40 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent, DialogNewPlayer } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatTableModule } from '@angular/material/table';
 import { MatButtonModule } from '@angular/material/button';
+import { MatRippleModule } from '@angular/material/core';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { MatNativeDateModule } from '@angular/material/core';
 
-@NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
+const MODULES = [
+  BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatTableModule,
-    MatButtonModule
+    MatButtonModule,
+    MatRippleModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatNativeDateModule,
+]
+@NgModule({
+  declarations: [
+    AppComponent,
+    DialogNewPlayer
   ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  imports: [...MODULES],
+  exports: [...MODULES],
   providers: [],
   bootstrap: [AppComponent]
 })
